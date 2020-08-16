b103eb1457530d28239e2ff945f043999ea4d1d3
mamadou alpha barry <mamadou.barry1105@gmail.com>
2020-08-16 21:43:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,17 @@
 # require modules here
 
 def load_library
-  # code goes here
+    # code goes here
+  data = YAML.load_file(file)
+  result = data.each_with_object({}) do |(icon, value),final_hash|
+    if !final_hash[icon]
+      final_hash[icon]= {
+        :english => value[0], 
+        :japanese => value[1]
+      }
+    end
+  result
+end
 end
 
 def get_japanese_emoticon
